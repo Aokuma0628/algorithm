b05b205d01885472d42623aeb75c433212fd0647
Aokuma0628 <otyazuke1192@gmail.com>
2022-09-21 16:53:56 +0900

    paiza A問題解答

---
+++ paiza/test/a066__.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+enum {
+  OK = 1,
+  NG = 0,
+  INPUT_ROW = 10000,
+  NUM_COUNT = 2,
+  MAX_LEN = 16,
+};
+
+static void main_logic(
+  int32_t num,
+  int32_t work[][NUM_COUNT]
+)
+{
+  int32_t max = 0;
+  int32_t cnt = 0;
+  int32_t m_cnt = 0;
+  int32_t i, j;
+  
+  for (i = 0; i < num; i++) {
+    if (max < work[i][1]) max = work[i][1];
+  }
+  
+  j = 1;
+  while (j <= max) {
+    bool in_value = false;
+
+    for (i = 0; i < num; i++) {
+      if (work[i][0] <= j && j <= work[i][1]) {
+        in_value = true;
+      }
+    }
+
+    if (in_value) {
+      cnt++;
+    }
+    else {
+      cnt = 0;
+    }
+
+    if (m_cnt < cnt) m_cnt = cnt;
+    j++;
+  }
+
+  printf("%d\n", m_cnt);
+
+  return;
+}
+
+int split_str2int(
+  char   *str,
+  int     num,
+  char    del,
+  int    *split
+)
+{
+  int   i   = 0;
+  char *tmp = str;
+  char *end = NULL;
+
+  if (!str || !split) {
+    return NG;
+  }
+
+  while(1) {
+    int val = strtol(tmp, &end, 10);
+    split[i] = val;
+
+    i++;
+    if(*end == '\0' || i >= num) {
+      break;
+    }
+
+    tmp = end;
+    tmp++;
+    end = NULL;
+  }
+
+  return OK;
+}
+
+
+int32_t main(void)
+{
+  char      str[MAX_LEN] = { 0 };
+  int32_t   work[INPUT_ROW][NUM_COUNT] = {{ 0 }};
+  int32_t   num = 0;
+  char     *tmp = NULL;
+  int32_t   i;
+
+  tmp = fgets(str, MAX_LEN, stdin);
+  num = strtol(str, NULL, 10);
+
+  for (i = 0; i < num; i++) {
+    memset(str, 0, MAX_LEN);
+    tmp = fgets(str, MAX_LEN, stdin);
+    split_str2int(str, NUM_COUNT, " ", work[i]);
+  }
+
+  main_logic(num, work);
+
+  return 0;
+}
